adf757f33b0fd12432c55601909c2f467f21162f
Franvalesi789 <fran.valesi@hotmail.com>
2022-02-17 12:20:19 -0300

    Encapsulamiento en proyecto lavadora

--- llfunciones/LLFunciones.java
+++ llfunciones/LLFunciones.java
@@ -11,4 +11,48 @@ public class LLFunciones {
         this.kilos = kilos;
         this.tipoDeRopa = tipoDeRopa;
     }
+
+    private void Llenado() {
+        if (kilos <= 12) {
+            llenadoCompleto = 1;
+            System.out.println("Llenando...");
+            System.out.println("Llenado completo!");
+        } else {
+            System.out.println("La carga de ropa es muy pesada, reduce la carga!");
+        }
+    }
+
+    private void Lavado() {
+        Llenado();
+        if (llenadoCompleto == 1) {
+            if (tipoDeRopa == 1) {
+                System.out.println("Ropa blanca / Lavado suave");
+                System.out.println("Lavando...");
+                lavadoCompleto = 1;
+            } else if (tipoDeRopa == 2) {
+                System.out.println("Ropa de color / Lavado intenso");
+                System.out.println("Lavando...");
+                lavadoCompleto = 1;
+            } else {
+                System.out.println("El tipo de ropa no esta disponbible!");
+                System.out.println("Se lavara como ropa de color / Lavado intenso");
+                lavadoCompleto = 1;
+            }
+        }
+    }
+
+    private void Secado() {
+        Lavado();
+        if (lavadoCompleto == 1) {
+            System.out.println("Secando...");
+            secadoCompleto = 1;
+        }
+    }
+
+    public void CicloFinalizado() {
+        Secado();
+        if (secadoCompleto == 1) {
+            System.out.println("Tu ropa esta lista!");
+        }
+    }
 }
