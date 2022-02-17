6886a7f18b513c7d2cd244ebe9ee0cde9d87387c
Franvalesi789 <fran.valesi@hotmail.com>
2022-02-17 11:17:54 -0300

    Encapsulamiento en java

---
+++ llfunciones/LLFunciones.java
@@ -0,0 +1,14 @@
+/*
+* Clase para las funciones logicas de una lavadora
+ */
+package llfunciones;
+
+public class LLFunciones {
+
+    private int kilos = 0, llenadoCompleto = 0, tipoDeRopa = 0, lavadoCompleto = 0, secadoCompleto = 0;
+
+    public LLFunciones(int kilos, int tipoDeRopa) {
+        this.kilos = kilos;
+        this.tipoDeRopa = tipoDeRopa;
+    }
+}
