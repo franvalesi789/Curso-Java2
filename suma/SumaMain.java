bd2036b29006370a7fc975f92b05072f034027e7
Franvalesi789 <fran.valesi@hotmail.com>
2022-02-16 23:14:46 -0300

    Programación Orientada a Objetos

---
+++ suma/SumaMain.java
@@ -0,0 +1,18 @@
+package suma;
+import java.util.Scanner;
+
+public class SumaMain {
+    public static void main(String args[]){
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Dame el primer valor: ");
+        int valorUno = entrada.nextInt();
+        
+        System.out.print("Dame el segundo valor: ");
+        int valorDos = entrada.nextInt();
+        
+        Suma valores = new Suma(valorUno, valorDos);
+        valores.Imprimir();
+    }
+}
