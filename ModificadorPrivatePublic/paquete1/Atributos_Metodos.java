bd25bcc6d9a09362a546fc25bd1ca2a4554af3b8
Franvalesi789 <fran.valesi@hotmail.com>
2022-02-17 14:45:01 -0300

    Modificadores Private/Public

---
+++ ModificadorPrivatePublic/paquete1/Atributos_Metodos.java
@@ -0,0 +1,20 @@
+package paquete1;
+
+public class Atributos_Metodos {
+
+    private int valorUno;
+    private int valorDos;
+    private String valorTres;
+
+    private void metodoFranco() {
+
+    }
+
+    public int getValorUno() {
+        return valorUno;
+    }
+
+    public void setValorUno(int valorUno) {
+        this.valorUno = valorUno;
+    }
+}
---
+++ ModificadorPrivatePublic/paquete2/ClaseJavaDos.java
@@ -0,0 +1,14 @@
+package paquete2;
+
+import paquete1.Atributos_Metodos;
+
+public class ClaseJavaDos {
+
+    public static void main(String[] args) {
+
+        Atributos_Metodos mensajero = new Atributos_Metodos();
+        mensajero.setValorUno(25);
+        System.out.println(mensajero.getValorUno());
+    }
+
+}
