e852d7545cbecfca230373d966d804183fd2611f
Franvalesi789 <fran.valesi@hotmail.com>
2022-02-17 14:12:36 -0300

    metodos setter y getter

--- lavadora_uno/Lavadora_Uno.java
+++ lavadora_uno/Lavadora_Uno.java
@@ -7,15 +7,17 @@ public class Lavadora_Uno {
 
     public static void main(String args[]) {
         Scanner entrada = new Scanner(System.in);
-        
+
         System.out.println("La ropa es blanca o de color?");
         System.out.println("Presiona 1 - ropa blanca / 2 - ropa color");
         int tipoDeRopa = entrada.nextInt();
-        
+
         System.out.println("Cuantos kilos de ropa?");
         int kilos = entrada.nextInt();
-        
+
         LLFunciones mensajero = new LLFunciones(kilos, tipoDeRopa);
+        mensajero.setTipoDeRopa(2);
+        System.out.println("El tipo de ropa es: " + mensajero.getTipoDeRopa());
         mensajero.CicloFinalizado();
     }
 }
