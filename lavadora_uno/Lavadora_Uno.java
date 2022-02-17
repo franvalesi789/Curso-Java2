de57f82dfc98e812cbbae8becb6001c697788105
Franvalesi789 <fran.valesi@hotmail.com>
2022-02-17 13:03:22 -0300

    Nuevo paquete, llamo metodo desde nueva clase

---
+++ lavadora_uno/Lavadora_Uno.java
@@ -0,0 +1,21 @@
+package lavadora_uno;
+
+import Libreria.LLFunciones;
+import java.util.Scanner;
+
+public class Lavadora_Uno {
+
+    public static void main(String args[]) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("La ropa es blanca o de color?");
+        System.out.println("Presiona 1 - ropa blanca / 2 - ropa color");
+        int tipoDeRopa = entrada.nextInt();
+        
+        System.out.println("Cuantos kilos de ropa?");
+        int kilos = entrada.nextInt();
+        
+        LLFunciones mensajero = new LLFunciones(kilos, tipoDeRopa);
+        mensajero.CicloFinalizado();
+    }
+}
