36a27f9b638ee87759a39c98d34e4245de8c62d8
Franvalesi789 <fran.valesi@hotmail.com>
2022-02-22 11:25:52 -0300

    Palabra reservada super

---
+++ Palabra_Super/ClasePrincipal.java
@@ -0,0 +1,9 @@
+package clases;
+
+public class ClasePrincipal {
+    public static void main(String[] args) {
+        
+        Hija mensajero = new Hija();
+        mensajero.Saludar();
+    }
+}
